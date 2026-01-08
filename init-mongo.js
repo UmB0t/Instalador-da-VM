f1f25c8cdc1eb2691f980c835205117cc1358fb3
Lorenzo Gabriel Barbosa Serradeira Cabral <141856862+UmB0t@users.noreply.github.com>
2026-01-07 22:09:53 -0300

    Delete init-mongo.js

--- init-mongo.js
+++
@@ -1,5 +0,0 @@
-db = db.getSiblingDB('asterisk_realtime');
-db.createCollection('extensions');
-db.createCollection('queues');
-db.createCollection('trunks');
-print(">>> Banco asterisk_realtime inicializado com sucesso!");
