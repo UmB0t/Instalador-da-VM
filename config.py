2e86d197ee3c820026faeb0c41904c4713162962
Lorenzo Gabriel Barbosa Serradeira Cabral <141856862+UmB0t@users.noreply.github.com>
2026-01-07 22:12:42 -0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,14 @@
+# Configurações de Banco de Dados
+DB_CONFIG = {
+    'dbname': 'asterisk',
+    'user': 'asterisk',
+    'password': 'asterisk',
+    'host': '127.0.0.1',
+    'port': '5432'
+}
+
+# Caminhos dos Arquivos
+FILES_CONFIG = {
+    'sip': '/etc/asterisk/sip_ramais.conf',
+    'pjsip': '/etc/asterisk/pjsip_ramais.conf'
+}
